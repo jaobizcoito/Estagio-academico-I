a251e57aa3338157bdee3296ca62f5ed2a4198c0
Pietra <pietra.barros@undf.edu.br>
2025-10-15 23:10:16 -0300

    Modificações feias em server

--- backend/server.js
+++ backend/server.js
@@ -6,23 +6,18 @@ const authRoutes = require('./routes/auth');
 const app = express();
 const PORT = 3000;
 
-// Middleware
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Servir arquivos estáticos (HTML, CSS, JS) da pasta "public"
 app.use(express.static('public'));
 
-// Rota raiz -> redireciona pro login
 app.get('/', (req, res) => {
   res.redirect('/login.html');
 });
 
-// Rotas de autenticação
 app.use('/auth', authRoutes);
 
-// Iniciar o servidor
 app.listen(PORT, () => {
-  console.log(`✅ Servidor rodando em http://localhost:${PORT}`);
+  console.log(`Servidor:http://localhost:${PORT}`);
 });
